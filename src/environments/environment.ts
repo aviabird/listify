98d3fc185162175d9e7193a47afabafac95b0937
voidzero <kartikjagdale11@gmail.com>
2017-01-19 12:34:59 +0530

    change env varibales and typescript version to 2.0.10

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,6 @@
 export const environment = {
   production: false,
   baseUrl: 'http://127.0.0.1:3000/api'
-  // baseUrl: 'https://istalk.herokuapp.com/api'
+  // baseUrl: 'https://listify-backend.herokuapp.com/api'
     
 };
